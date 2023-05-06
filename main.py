55e2f90bc9e45798a63fc88e753afe3bc97cc66f
Serhat Esen <serhat@devstudio.design>
2023-05-06 15:07:03 +0300

    for user

--- main.py
+++ main.py
@@ -6,9 +6,19 @@ Asal sayılar : 1'e ve kendisinden başka bir sayıya bölünmeyen sayılara den
 
 def asalnumber(num):
     if num < 2:   # 1 ve daha küçük sayılar asal değil
-        return "Asal sayı değildir"
+        return False
     for i in range(2, num):
         if num % i == 0:
-            return "Asal sayı değildir"
-    return "Asal sayıdır"
+            return False
+    return True
 
+while True:
+    number = input('Sayı: ')
+    if (number == 'q'):
+        break
+    else:
+        number = int(number)
+        if (asalnumber(number)):
+            print(number, 'Asal Sayıdır')
+        else:
+            print(number, 'Asal sayı değildir')
